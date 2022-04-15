f0fd5ab019fc067ab95fe9fe8a260303fa04e87f
Jeffrey Ben <jeffreyben@Jeffreys-MBP-3.hitronhub.home>
2022-04-15 14:44:51 -0400

    edf-algorithm

---
+++ edf/Main.java
@@ -0,0 +1,19 @@
+package edf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Main {
+    public void main(String[] args) {
+        List<Task> taskListPassedIn = new ArrayList<Task>();
+        Task one = new Task(2,4);
+        Task two = new Task(3,6);
+        Task three = new Task(1,5); 
+    
+        taskListPassedIn.add(one);
+        taskListPassedIn.add(two);
+        taskListPassedIn.add(three);
+
+        Scheduler.schedule(taskListPassedIn);
+    }
+}
